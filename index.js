4b083bbd76894b46c37589b55840af3a37559d39
highscoretech <highscoretechng@gmail.com>
2024-03-10 06:36:45 -0600

    flip

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
+require("dotenv").config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+
 app.get("/", (req, res)=>{
-    res.status(200).json({perfect:"Welcome to Wager server"})
+    res.status(200).json({perfect:"Welcome to Wager.io"})
   })
+
+app.get("/profile", (req, res)=>{
+res.status(200).json({user:"My name is Valiant"})
+})
+
 
 const PORT = process.env.PORT || 5000; 
 
